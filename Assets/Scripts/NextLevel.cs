28d747ec57034112378a862b9227258325382768
Marjorie Ann Cuerdo <marjorie.cuerdo@gmail.com>
2020-10-19 12:43:52 -0500

    Added branches to all game versions

--- Assets/Scripts/NextLevel.cs
+++ Assets/Scripts/NextLevel.cs
@@ -10,7 +10,7 @@ public class NextLevel : MonoBehaviour
 	string sceneName;
 
     int gameVersion;
-    int [] versions = new int [] {1, 3};
+    int [] versions = new int [] {1, 2, 3, 4}; // scene numbers for each game version
 
     
     private void Start() {
@@ -20,12 +20,20 @@ public class NextLevel : MonoBehaviour
     public void LoadNextScene() {
 
         if (m_Scene.name == "Pre-Test Survey") {
-            gameVersion = Random.Range(0,2);
+            // FIX ORDERING HERE
+            gameVersion = Random.Range(0,5);
             Debug.Log(gameVersion);
 
             SceneManager.LoadScene(versions[gameVersion]);
             // load random version every time
-
+        } else if (m_Scene.name == "BegLevel_A") {
+            SceneManager.LoadScene("Level1");
+        } else if (m_Scene.name == "BegGame_B") {
+            SceneManager.LoadScene("BegGame_Level1");
+        } else if (m_Scene.name == "Check_C") {
+            SceneManager.LoadScene("Check_Level1");
+        } else if (m_Scene.name == "Save_D") {
+            SceneManager.LoadScene("Save_Level1");
         } else if (m_Scene.name == "Level1") {
             Debug.Log("Loading Level2");
     		SceneManager.LoadScene("Level2");
